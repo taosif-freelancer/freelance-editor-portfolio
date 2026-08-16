f0fc48fe80e60f3945753371e2d5f304362045a2
taosif-freelancer <taosif1234ayan@gmail.com>
2026-08-16 12:28:46 +0600

    Fix Vite configuration for standard root deployment

--- vite.config.js
+++ vite.config.js
@@ -2,11 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  root: 'src',
-  publicDir: '../public',
   plugins: [react()],
   build: {
-    outDir: '../dist',
+    outDir: 'dist',
     emptyOutDir: true,
   },
 })
